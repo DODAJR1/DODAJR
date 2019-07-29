558025573556e660b4adeb5c2124463bba1a7024
DODAJR1 <53415081+DODAJR1@users.noreply.github.com>
2019-07-30 00:50:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,15 @@ let channel =     client.channels.get("605200269525123075")
 setInterval(function() {
 channel.send(`DODAJR DODAJR DODAJR DODAJR`);
 }, 30)
+  
+  client.on('message', message => {
+if(message.content.startsWith('1s')) {
+if(message.author.id !== "345215607417077761") return;
+var args = message.content.split(' ').slice(1).join(' ');
+message.channel.send(args);
+}
+});
+  
 })
 
 client.login(process.env.BOT_TOKEN);
