dcbae9eb125d301c8892db3d42fd17fc32b6d5c7
DODAJR1 <53415081+DODAJR1@users.noreply.github.com>
2019-08-03 16:51:40 +0200

    Update bot10.js

--- bot10.js
+++ bot10.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "601331176418050074"; // ايدي السررفر
-var channel = "601362400079380510";//ايدي الروم
+var server = "605200268451119143"; // ايدي السررفر
+var channel = "605200269525123075";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('** محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد **')
     },305);
@@ -16,25 +16,25 @@ var channel = "601362400079380510";//ايدي الروم
 
 
 client.on('ready', async() => {
-var server = "601331176418050074"; // ايدي السررفر
-var channel = "601362426797228066";//ايدي الروم
+var server = "605200268451119143"; // ايدي السررفر
+var channel = "605200269525123075";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('#daily')
     },8.64e+7);
 })
 
 client.on('ready', async() => {
-var server = "601331176418050074"; // ايدي السررفر
-var channel = "601362400079380510";//ايدي الروم
+var server = "605200268451119143"; // ايدي السررفر
+var channel = "605200269525123075";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#rep <@306464381775118336>')
+    client.guilds.get(server).channels.get(channel).send('#rep <@345215607417077761>')
     },8.64e+7);
 })
 
 
 client.on('message', message => {
 if(message.content.startsWith('10s')) {
-if(message.author.id !== "306464381775118336") return;
+if(message.author.id !== "345215607417077761") return;
 var args = message.content.split(' ').slice(1).join(' ');
 message.channel.send(args);
 }
@@ -49,13 +49,13 @@ client.on('message', message => {
 
 client.on('message', message => {
 	if(message.content === '2'){
-        message.channel.send('#credit')
+        message.channel.send('#credits')
     }
 });
 
 client.on('message', message => {
 	if(message.content === '3'){
-        message.channel.send('#rep <@306464381775118336>')
+        message.channel.send('#rep <@345215607417077761>')
     }
 });
 
